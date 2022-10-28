9b27fae15cd4e88b79bbd8148e6a05b175289e86
“Nurlan” <ikhsan.nurlan@gmail.com>
2022-10-28 16:52:22 +0600

    added: file auth.go

--- x/did/types/auth.go
+++ x/did/types/auth.go
@@ -1,3 +1,43 @@
 package types
 
+import (
+	"fmt"
+	sdkcodec "github.com/cosmos/cosmos-sdk/codec"
+	"github.com/tendermint/tendermint/crypto"
+)
+
+
+func Sign(signableData sdkcodec.ProtoMarshaler, seq uint64,privKey crypto.PrivKey) ([]byte, error){
+	return privKey.Sign(mustGetSignBytesWithSeq(signableData, seq))
+}
+
+func Verify(signature []byte, signableData sdkcodec.ProtoMarshaler,seq uint64, pubKey crypto.PubKey) (uint64,bool) {
+	signBytes := mustGetSignBytesWithSeq(signableData, seq)
+	
+	if !pubKey.VerifySignature(signBytes, signature) {
+		return 0,false
+	}
+	return nextSequence(seq), true
+}
+
+func mustGetSignBytesWithSeq(signableData sdkcodec.ProtoMarshaler, seq uint64) []byte {
+	dAtA, err := signableData.Marshal()
+	if err != nil {
+		panic(fmt.Sprintf("marshal failed: %s, signableData: %s", err.Error(), signableData))
+	}
+	dataWithSeq := DataWithSeq{
+		Data: dAtA,
+		Sequence: seq,
+	}
+	dAtA, err = dataWithSeq.Marshal()
+	if err != nil {
+		panic(fmt.Sprintf("marshal failed: %s, dataWithSeq:%v", err.Error(), dataWithSeq))
+	}
+	return dAtA
+}
+
 const InitialSequence uint64 = 0
+
+func nextSequence(seq uint64) uint64 {
+	return seq + 1
+}
